f5d6104f87e90eaa61bde8446142b0b44e555da4
cozmoguy <milanjohntngl@gmail.com>
2020-07-13 22:59:50 +0530

    first

---
+++ scraping_script.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+
+
+browser = webdriver.Chrome()
+browser.get('https://en.wikipedia.org/wiki/List_of_effects')
+
+for i in range(27):
+    effects = browser.find_elements_by_xpath(f'//*[@id="mw-content-text"]/div/ul[{i}]/li')
+    for x in range(len(effects)):
+        effect = effects[x].text
+        neweffect = effect.replace(')','')
+        effectlist = neweffect.split('(')
+        effectresult = {'Title': 'title','Keywords':[]}
+        for y in range(len(effectlist)):
+            if y == 0:
+                 effectresult['Title'] = effectlist[0]
+            else:
+                effectresult['Keywords'].append(effectlist[y])
+        print(effectresult)            
+
+        
+
+
+browser.close()
+
